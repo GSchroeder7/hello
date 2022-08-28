f93046a8103e31d6d7e70a9c4d8a7915e0a06bf9
GSchroeder7 <gschroeder7@huskers.unl.edu>
2022-08-28 00:10:28 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,6 +1,9 @@
+//importing library
+#include <stdio.h>
+
 //creating a basic function to print my name
 int main() {
-    printf("Griffin Schroeder\n");
-    printf("My Major is: Computer Science!");
+    printf("My Name is Griffin Schroeder!\n");
+    printf("My Major is Computer Science!\n");
     return 0;
 }
